1ed99496dab346a2e8969e9205476452d68bdcb3
TyrYoxan <clem.bri25@gmail.com>
2025-03-10 15:19:57 +0100

    Singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,22 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config {
+    private static $_settings;
+
+    public static function getSetting(){
+        if(is_null(self::$_settings)){
+            self::$_settings = new Config();
+        }
+        return self::$_settings;
+    }
+    private function __construct(){
+        self::getSetting();
+
+    }
+
+    public function getValue($key){
+        return self::$_settings[$key];
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,7 +1,17 @@
 <?php
+
+use App\Config;
+
 require('../vendor/autoload.php');
 
 
 # TODO: Récuperer une instance de Config
+
+$config = Config::getSetting();
+
 # Afficher une valeur contenu dans config.php
+echo $config->getValue('db')['host'];
+
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+$config2 = Config::getSetting();
+echo ($config === $config2);
